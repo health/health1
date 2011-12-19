e19cf30fd96916cd03719d81be6c833366238414
health <w21winner@gmail.com>
2011-12-19 16:41:53 +0200

    m1 commit 191211

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+M1::Application.config.secret_token = '417db288550d30e3160999f091414107602cadcda73af87a8bae41ff0eabc5c1ce2252d20ef68709ae20c8a26658eaa535e5931e11c15b937fd57120d624b10f'
